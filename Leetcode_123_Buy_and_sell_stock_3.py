66c1907658bff944b9b7be355b6bfa8e6813bc78
ankitkrishan <70434125+ankitkrishan@users.noreply.github.com>
2021-12-29 17:56:00 +0530

    Create Leetcode_123_Buy_and_sell_stock_3.py

---
+++ Leetcode_123_Buy_and_sell_stock_3.py
@@ -0,0 +1,16 @@
+def solve(current_index: int, prices: list, buy: bool, transaction_count: int, mp):
+    if current_index >= len(prices) or transaction_count == 2:
+        return 0
+    currentKey = (transaction_count, current_index, buy)
+    if currentKey in mp:
+        return mp[currentKey]
+    if not buy:
+        add = -prices[current_index] + solve(current_index + 1, prices, True, transaction_count, mp)
+    else:
+        add = prices[current_index] + solve(current_index + 1, prices, False, transaction_count + 1,mp)
+    notadd = solve(current_index + 1, prices, buy, transaction_count, mp)
+    mp[currentKey] = max(add, notadd)
+    return mp[currentKey]
+class Solution:
+    def maxProfit(self, prices) -> int:
+        return solve(0, prices, False, 0, {})
